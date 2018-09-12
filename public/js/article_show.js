91d7d21de11ade7a380c3d2cca53669a2de3cf48
tdimnet <thomas.dimnet@gmail.com>
2018-09-12 18:55:01 +0200

    Fix error with JS script: article can now be toggle.

--- public/js/article_show.js
+++ public/js/article_show.js
@@ -1,8 +1,10 @@
 $(document).ready(function() {
-  $('.js-like-article-count').on('click', function(e) {
+  $('.js-like-article').on('click', function(e) {
     e.preventDefault();
+
     var $link = $(e.currentTarget);
     $link.toggleClass('fa-heart-o').toggleClass('fa-heart');
+
     $('js-like-article-count').html('TEST');
   });
 });
