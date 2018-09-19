45905078859b00780a4fcc1614652d295bd038a0
tdimnet <thomas.dimnet@gmail.com>
2018-09-19 18:10:29 +0200

    Remove useless dump

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -54,9 +54,6 @@ EOF;
         }
         $articleContent = $item->get();
 
-        dump($markdown);die;
-
-
         return $this->render('article/show.html.twig', [
             'articleContent' => $articleContent,
             'comments' => $comments,
